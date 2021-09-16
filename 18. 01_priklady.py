32cb566d66d732c6235b49fc4dd6554f9fafe3da
lap777777 <verzus.mining@gmail.com>
2021-09-16 18:05:11 +0200

    pridany metody z collections

---
+++ 18. 01_priklady.py
@@ -0,0 +1,17 @@
+# https://github.com/trohat/python1-kurz/blob/main/OOP1/OOP-1.txt
+
+# Úkol - flexibilní slovník
+
+# Vytvořte třídu FlexibilniSlovnik, která nerozlišuje mezi inty a stringy v rolích klíčů.
+
+# Pokud ve slovníku vytvořím položku s klíčem 1 (číslo), je to něco jiného, než když vytvořím položku s klíčem '1' (string).
+# Pro vaši novou třídu to ale bude stejný klíč. Takže když se pokusím přečíst hodnotu s klíčem 1, slovník zkusí najít všechny možnosti (jedna jako int, i jedna jako string), než se vzdá a ohlásí, že klíč neexistuje.
+
+# Příklad:
+# fs = FlexibilniSlovnik()
+# fs[1] = "jablko"
+# print(fs['1']) -> vytiskne "jablko"  (normálně by byla KeyError)
+# fs['2'] = hruška
+# print(fs[2]) -> vytiskne "hruška" (normálně by opět byla KeyError)
+
+# Všechno ostatní funguje jako v běžném slovníku.
--- 18_Lekce_Exceptions.py
+++ 18_Lekce_Exceptions.py
@@ -29,7 +29,7 @@ print("Program pokracuje.")
 class TooManyEmployersException(Exception):
     pass
 # vytvoreni vlastni vyjimky na zaklade tridy Exception
-# str je tam automaticky z tridy exception
+# __str__ je tam automaticky z tridy exception
 # takze ji definuji prazdnou
 
 employers = 500
@@ -39,11 +39,11 @@ def delej_neco_jineho():
         raise TooManyEmployersException
 
 def delej_neco():
-    delej_neco_jineho()
+    delej_neco_jineho() 
 
 try:
     delej_neco() # pokud chyba, tak spadne tato funkce a vnorena funkce, vyhodi se vyjimka nize a program pokracuje.
-    # mam misto, kde chybu kontroluji a jine misto, kde ji vytisknu
+    # tady mam misto, kde chybu kontroluji a dale mam jine misto, kde ji vytisknu
 
 except TooManyEmployersException: 
     # a teprve tady mi to vyhodi vyjimku
@@ -66,3 +66,38 @@ except PrilosMnohoKopeckuException as e:
     print(e)
 """
 
+
+### Collections ####################################################
+
+# 1) Country - pocitadlo 
+
+from collections  import Counter
+
+c = Counter("abcdefghaaa")
+print(c)
+print()
+
+cnt = Counter()
+text = "Sla nanynka do zeli, natrhala lupeni a pak sla domu a vsechno to snedla."
+my_list = text.split(" ")
+for word in my_list:
+    cnt[word] += 1
+print(cnt)
+print()
+
+# 2) defaultDict
+# dictionary subclass that calls a factory function to supply missing values
+# slovnik ktery vrati factory argument jako hodnotu a nevrati KeyError pro chybejici klic
+
+from collections import defaultdict
+
+# lambda as default_factory argument
+d = defaultdict(lambda: "Not present")
+
+d["a"] = 1
+d["b"] = 2
+ 
+print(d)
+print(d["a"])
+print(d["b"])
+print(d["c"])
